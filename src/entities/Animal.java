7bbd88209d23f0c8a72142d91566174da11c9f15
rmwiesenberg <rmwiesenberg@wpi.edu>
2017-11-07 21:09:30 -0500

    maybe we should have animals

---
+++ src/entities/Animal.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class Animal extends absSubject {
+	private int id;
+
+	public Animal(String name, int id, float[] position, Inventory inventory, 
+			PhysicalHealth physHealth) {
+		super(name, position, inventory, physHealth);
+		this.id = id;
+	}
+
+	public Animal(String name, int id, float[] position, int slots, 
+			PhysicalHealth physHealth) {
+		super(name, position, slots, physHealth);
+		this.id = id;
+	}
+
+}
