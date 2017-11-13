5ca7808ba9fb8b51c4ab77a5fd5598bca68938e8
Tyler <tystorr@gmail.com>
2017-11-13 15:12:43 -0500

    added basic handles for glfw window

---
+++ src/boundary/Main.java
@@ -0,0 +1,15 @@
+package boundary;
+
+import boundary.renderEngine.DisplayManager;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DisplayManager window = new DisplayManager();
+		window.init();
+		window.loop();
+		window.terminate();
+
+	}
+
+}
