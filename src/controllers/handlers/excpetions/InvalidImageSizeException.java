40d0a751538dbcc65bc68553496dfaef1debdd44
rmwiesenberg <rmwiesenberg@wpi.edu>
2017-11-15 23:02:53 -0500

    Finish Voxel Handler

---
+++ src/controllers/handlers/excpetions/InvalidImageSizeException.java
@@ -0,0 +1,34 @@
+package controllers.handlers.excpetions;
+
+public class InvalidImageSizeException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6935874430372808783L;
+
+	public InvalidImageSizeException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidImageSizeException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidImageSizeException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidImageSizeException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidImageSizeException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
