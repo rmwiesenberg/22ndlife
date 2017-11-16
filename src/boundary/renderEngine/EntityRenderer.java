0dd41cc8d2a00f9ebb3c1a7402d8907e900bbdfe
Tyler <tystorr@gmail.com>
2017-11-16 01:44:47 -0500

    refactor rendering

--- src/boundary/renderEngine/EntityRenderer.java
+++ src/boundary/renderEngine/EntityRenderer.java
@@ -1,5 +1,7 @@
 package boundary.renderEngine;
 
+import java.util.ArrayList;
+
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL20;
@@ -11,27 +13,35 @@ import boundary.models.TexturedModel;
 public class EntityRenderer {
 	
 	public static void render(Frame frame) {
-		int[] vaos = frame.getVAOs();
-		for(int i = 0; i < vaos.length; i++) {
-			int vaoID = vaos[i];
+		ArrayList<Integer> vaoIDs = frame.getVAOs();
+		for(int vao: vaoIDs) {
 			// Get used VAO
-			GL30.glBindVertexArray(frame.getVertexArray(vaoID));
+			GL30.glBindVertexArray(frame.getVertexArray(vao));
+			
 			// Enable first value of array to be read "vertex data"
 			GL20.glEnableVertexAttribArray(0);
+			
 			// Enable first value of array to be read "texture data"
 			GL20.glEnableVertexAttribArray(1);
+			
 			// Enable texture bank
 			GL13.glActiveTexture(GL13.GL_TEXTURE0);
+			
 			// 2D texture ------
-			GL11.glBindTexture(GL11.GL_TEXTURE_2D, frame.getTexture(vaoID));
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, frame.getTexture(vao));
+			
 			// Draw Triangles based on model data vertex count
-			GL11.nglDrawElements(GL11.GL_TRIANGLES, frame.getVertexCount(vaoID), GL11.GL_UNSIGNED_INT, 0);
+			GL11.nglDrawElements(GL11.GL_TRIANGLES, frame.getVertexCount(vao), GL11.GL_UNSIGNED_INT, 0);
+			
 			// Unbind VBO from render data "vertex data"
 			GL20.glDisableVertexAttribArray(0);
+			
 			// Unbind VBO from render data "texture data"
 			GL20.glDisableVertexAttribArray(1);
+			
 			// Unbind VAO from render data
 			GL30.glBindVertexArray(0);
+			frame.cleanUp();
 		}		
 	}
 }
