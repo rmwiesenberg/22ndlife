4a385f2af248be5bd5cc70dfda9f31dcd0ad40d8
rmwiesenberg <rmwiesenberg@wpi.edu>
2017-11-15 23:11:33 -0500

    Javadocs for ImageHandler

--- src/controllers/handlers/ImageHandler.java
+++ src/controllers/handlers/ImageHandler.java
@@ -8,7 +8,15 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class ImageHandler {
-	public static float[] convertInt2FloatArray(int color) {
+	
+	/**
+	 * Returns the float array derived from a single integer
+	 * 
+	 * 
+	 * @param color [blue, green, red, alpha] bitwise added to the int
+	 * @return float array of 0..1
+	 */
+	public static float[] convertInt2FloatArray(int color) {		
 		float[] output = new float[4];
 		for(int i = 0; i < 4; i++) {
 			output[i] = ((float) ((color >> 8*i) & 0xff)) / 255.0f;
@@ -16,6 +24,13 @@ public class ImageHandler {
 		return output;
 	}
 	
+	/**
+	 * Returns an int array array with color values bitwise added into an int
+	 * [blue, green, red, alpha] with the indexes representing int[y][x] 
+	 * 
+	 * @param filepath absolute or project relative path of image
+	 * @return the int array
+	 */
 	public static int[][] convertPNG(String filepath) {
 		BufferedImage image = null;
 		try {
@@ -23,11 +38,13 @@ public class ImageHandler {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		// transfer image and create canvas
 	    final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
 	    final int width = image.getWidth();
 	    final int height = image.getHeight();
 	    final boolean hasAlphaChannel = image.getAlphaRaster() != null;
 	
+	    // handle alpha channel and int parsing
 	    int[][] result = new int[height][width];
 	    if (hasAlphaChannel) {
 	       final int pixelLength = 4;
