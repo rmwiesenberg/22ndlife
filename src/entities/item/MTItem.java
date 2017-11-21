d7f9aa6ce26a595e9a4b3f5998c45f66ab4a983b
rmwiesenberg <rmwiesenberg@wpi.edu>
2017-11-20 19:45:23 -0500

    Entities StarUML

--- src/entities/item/MTItem.java
+++ src/entities/item/MTItem.java
@@ -4,14 +4,17 @@ public class MTItem implements IItem {
 
 	public MTItem() {}
 	
+	@Override
 	public int getID() {
 		return 0;
 	}
 	
+	@Override
 	public String getName() {
 		return "empty";
 	}
 	
+	@Override
 	public int getMaxSize() {
 		return 0;
 	}
