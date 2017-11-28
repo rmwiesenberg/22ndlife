9fc7800c35b155a0cef2434f1f42847098eb8469
rmwiesenberg <rmwiesenberg@wpi.edu>
2017-11-28 01:25:20 -0500

    camera added

--- src/entities/world/Camera.java
+++ src/entities/world/Camera.java
@@ -1,5 +1,22 @@
 package entities.world;
 
-public class Camera {
+import org.joml.Vector3f;
 
+public class Camera {
+	private Vector3f pos;
+	private Vector3f rot;
+	
+	public Camera(Vector3f pos, Vector3f rot) {
+		this.pos = pos;
+		this.rot = rot;
+	}
+
+	// Getters and Setters
+	public Vector3f getPos() {
+		return pos;
+	}
+
+	public Vector3f getRot() {
+		return rot;
+	}
 }
